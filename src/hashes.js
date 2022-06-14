7924530ebf4c15d6510a705bdcff1e5ab511ec5f
Tadeas Kmenta <tadeas.kmenta@zel.cash>
2022-06-14 22:08:52 +0700

    update hashes

--- src/hashes.js
+++ src/hashes.js
@@ -2,6 +2,7 @@ function getHashes() {
   return [
     'ff0474a5337887bf7420ab074e92a313',
     'adeddd6909e3db3e43ac3392cfd20213',
+    'b81fa257301e6ad78e3809ced588bf7d',
   ];
 }
 module.exports = {
