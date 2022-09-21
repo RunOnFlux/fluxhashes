7108b2f068d987165b033c5eb2b4e8eac017c551
TheTrunk <tadeas.kmenta@zel.cash>
2022-09-21 10:24:49 +0700

    update hashes

--- src/hashes.js
+++ src/hashes.js
@@ -2,6 +2,7 @@ function getHashes() {
   return [
     'ec2c53860e431b8cb3b3a95bdfba0dbe',
     'c869decfedac6ef8818fcdec01df9547',
+    '2296d9bd25f877cb2cab8dd3c4229207',
   ];
 }
 module.exports = {
