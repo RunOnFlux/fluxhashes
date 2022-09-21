1f56017c19b8c84098dd601f8fdf723072f853dc
TheTrunk <tadeas.kmenta@zel.cash>
2022-09-21 16:55:26 +0700

    update hashes

--- src/hashes.js
+++ src/hashes.js
@@ -2,7 +2,7 @@ function getHashes() {
   return [
     'ec2c53860e431b8cb3b3a95bdfba0dbe',
     'c869decfedac6ef8818fcdec01df9547',
-    'c8cbfc9864d55b716aba70a5b78e405f',
+    '142a6c6c5af8d137a0b6f411c869a0f3',
   ];
 }
 module.exports = {
