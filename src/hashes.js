23c0deeeb3cc1e6639b36088387eca09e60e1c45
TheTrunk <tadeas.kmenta@zel.cash>
2022-09-26 16:36:53 +0700

    update hashes

--- src/hashes.js
+++ src/hashes.js
@@ -2,7 +2,7 @@ function getHashes() {
   return [
     'ec2c53860e431b8cb3b3a95bdfba0dbe',
     'c869decfedac6ef8818fcdec01df9547',
-    '48f6b19350ee81da9e05fa99adc2c94c',
+    '55145d9df85ce4ca73dc0d646d22cff9',
   ];
 }
 module.exports = {
