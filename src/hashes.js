888398f3511047b59f4f3aadd0a417db0abf9050
TheTrunk <tadeas.kmenta@zel.cash>
2022-10-16 09:44:26 +0700

    update hashes

--- src/hashes.js
+++ src/hashes.js
@@ -3,6 +3,7 @@ function getHashes() {
     'ec2c53860e431b8cb3b3a95bdfba0dbe',
     'c869decfedac6ef8818fcdec01df9547',
     '8f9e90e9f85014b92575e1a58ee986d0',
+    '1a4090c6715cf59bffa648724ca1a786',
   ];
 }
 module.exports = {
