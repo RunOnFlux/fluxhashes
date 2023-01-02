9455e339d68f250afef375710661fa4652c82136
TheTrunk <tadeas.kmenta@zel.cash>
2023-01-02 17:49:58 +0700

    update hashes

--- src/hashes.js
+++ src/hashes.js
@@ -5,7 +5,7 @@ function getHashes() {
     '79f25c3a3bcc855096a10af1086f4024',
     'eee0ed971fe1f4f297a824b6167ce594',
     '9757b7f801b1e228a50a3544a2b82c69',
-    'e592c6d36c3a1f73ee8d367d3e13ec60',
+    'c9f9406afc9bee61ff78bc1dfe13cb20',
   ];
 }
 module.exports = {
