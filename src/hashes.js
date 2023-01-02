cfec7f950dcfa6b1349b605d2c6e6142accc7a52
TheTrunk <tadeas.kmenta@zel.cash>
2023-01-02 17:11:58 +0700

    update hashes

--- src/hashes.js
+++ src/hashes.js
@@ -15,7 +15,7 @@ function getHashes() {
     '79f25c3a3bcc855096a10af1086f4024',
     'eee0ed971fe1f4f297a824b6167ce594',
     '9757b7f801b1e228a50a3544a2b82c69',
-    '57809996d63e9143f31d1b66c5663858',
+    'e592c6d36c3a1f73ee8d367d3e13ec60',
   ];
 }
 module.exports = {
