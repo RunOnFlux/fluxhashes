b43c6a9c214b1ae5f6c440093bb06ce43f4f476e
TheTrunk <TheTrunk@users.noreply.github.com>
2023-01-20 08:43:49 +0700

    Update hashes.js

--- src/hashes/hashes.js
+++ src/hashes/hashes.js
@@ -1,12 +1,5 @@
 function getHashes() {
   return [
-    'eee0ed971fe1f4f297a824b6167ce594',
-    '9757b7f801b1e228a50a3544a2b82c69',
-    'c9f9406afc9bee61ff78bc1dfe13cb20',
-    'a8a99cd11043b1a0bdf1e2cc28a48881',
-    'df623fb8751f73d640afd1018ef4e947',
-    'bb65b6f87348a4027a250152061c0901',
-    '697acd3fa6471e50b3975d9e6d14a871',
     '0e38503355ab2e5f9a5c9e90c397b0e1',
     '50ec6d1facee9e61202cf64e8c50b63a',
     '63c8ec2243898a12140ab15707c7e96a',
