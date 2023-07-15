cb8cb6b94527d7e6a175323131d088cc45d7dd15
TheTrunk <tadeas.kmenta@zel.cash>
2023-07-15 22:33:40 +0700

    update hashes

--- src/hashes/hashes.js
+++ src/hashes/hashes.js
@@ -1,6 +1,5 @@
 function getHashes() {
   return [
-    'a04340c7f28d5b9031ae64c0e4fdf8c7',
     'b2c23f7ee37655e7ba4594d6b4d7938e',
     'fbdee6829f10c1603a321c77abe6e0f2',
     'f880b489566d692018d67428ec6e27de',
