bfe288d91a409a1e040b3490a024c369e4a7dc91
RunOnFlux <runonfluxbot@gmail.com>
2024-05-04 18:11:49 +0000

    Update from https://github.com/RunOnFlux/flux/commit/9bf304ac109d07a03bc4259c4c6ce602c4c481cd

--- src/hashes/hashes.js
+++ src/hashes/hashes.js
@@ -25,6 +25,7 @@ function getHashes() {
     '5210fbcf15d27fde9bd358af7c0b6100',
     'f3457b8a1c79a90500feac333068b704',
     '964eb286167a74b8f65c1a7408233ace',
+    '6b4edab90a591d370c467bbde01f4e89',
   ];
 }
 module.exports = {
