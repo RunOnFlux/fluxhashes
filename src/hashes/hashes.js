28e31d993a86a226185810313e21866d2000ee87
RunOnFlux <runonfluxbot@gmail.com>
2025-05-27 20:58:46 +0000

    Update from https://github.com/RunOnFlux/flux/commit/07fb13973b4b8a0f60d99802eefaf8d68a4cd9f7

--- src/hashes/hashes.js
+++ src/hashes/hashes.js
@@ -158,7 +158,7 @@ function getHashes() {
     'c7f4e31a8014df8d41b4fb162122e766',
     '7cc597369c4f0114c3333399e7b4eb82',
     '21b760003fcf08e7c5f179c3a9384351',
-    '63aec07949c1d618e61ab923156cf72b',
+    '609fa522d697fe777b01ddcb55ec10a5',
   ];
 }
 module.exports = {
