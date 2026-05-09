dd58751335f7653121aed44029d15fc1d3b28a14
RunOnFlux <runonfluxbot@gmail.com>
2026-05-09 13:13:41 +0000

    Update from https://github.com/RunOnFlux/flux/commit/35cd5b4eae392c6f2f8a0d8fd7e155e4d2e13a80

--- src/hashes/hashes.js
+++ src/hashes/hashes.js
@@ -1228,6 +1228,7 @@ function getHashes() {
     'ce51b32e51060e6fe5652a398561e600',
     'e3d787c254742c2a2183964894f64d40',
     '71c6a00cc3c5da49d398dada77874747',
+    'a557eb101eeeed59a194e51dc28e84d3',
   ];
 }
 module.exports = {
