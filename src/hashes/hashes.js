efccf6ac71e957b662a04ca142f8f4ee3564e98d
hashlist-signer <runonfluxbot@gmail.com>
2026-09-21 15:25:53 +0000

    Sign hash list seq 89

--- src/hashes/hashes.js
+++ src/hashes/hashes.js
@@ -365,6 +365,7 @@ function getHashes() {
     'a3fba4dbfc8a735f91a7a17f2ab290b0',
     '405805accd3e95d0e73e44409abbde27',
     'f5e2ee596cbf3eaf931c7a671eabded0',
+    '1c9faf90caa5fb6e7df71a2d09648c89',
   ];
 }
 
